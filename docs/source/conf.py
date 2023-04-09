fef2bf6e8b512567e841b03505014f63d12435a5
hejixin <hejinxin_@outlook.com>
2023-04-09 23:02:50 +0800

    M

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,22 @@ extensions = [
     'myst_parser',
 ]
 
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
 
 extensions.append('sphinx.ext.todo')
 extensions.append('sphinx.ext.autodoc')
